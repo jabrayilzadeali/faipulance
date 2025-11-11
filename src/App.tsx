041a03d5ba5e2b3eaeae163cb8425c3df5f53d6c
Jabrayilzade Ali <jabrayilzadeali@protonmail.com>
2025-11-11 19:39:16 +0400

    react, tailwindcss, shadcn ui, react router setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import { Button } from "@/components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      The App page
+      <Button variant="link">Button</Button>
+    </>
+  )
+}
+
+export default App
--- src/main.ts
+++
@@ -1,4 +0,0 @@
-import './style.css'
-import init from './index/app'
-
-init()
